abe312d0fc6ce4c42a5cb1a2edff82ad48c0de5e
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-02-10 17:53:55 +0700

    optimalisasi Image with next

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "../styles/About.module.css";
 import Layout from "../layouts/mainLayout";
 
@@ -7,6 +8,12 @@ export default function about() {
       <div className="text-center p-4">
         <h1 className="text-2xl text-blue-400">Welcome To About Page</h1>
         <h2 className={styles["title-h1"]}>Is about everything</h2>
+        <Image
+          src="/img/itzy.jpg"
+          width={400}
+          height={220}
+          alt="about"
+        />
       </div>
     </Layout>
   );
