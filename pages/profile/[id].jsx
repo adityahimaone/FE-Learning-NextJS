bf9d9e78b72ee7573bb10d80c36523d0ae745770
Aditya Himawan <30428194+adityahimaone@users.noreply.github.com>
2022-02-10 22:20:40 +0700

    get static props & static path

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -1,13 +1,74 @@
 import { useRouter } from "next/router";
 import Layout from "../../layouts/mainLayout";
 
-export default function ProfilDynamic() {
+export default function ProfilDynamic(props) {
+  const { UserData } = props;
   const router = useRouter();
   const { id } = router.query;
 
+  console.log(UserData);
   return (
     <Layout>
-      <h1>Profile Detail {id}</h1>
+      {/* add back button */}
+
+      <h1 className="text-center text-xl font-semibold p-4">
+        Profile Detail {id}
+      </h1>
+      <div className="m-4 p-2 backdrop-blur-sm bg-blue-300 rounded-md text-white">
+        <div className="flex">
+          <p>Name </p>
+          <p>: {UserData.name}</p>
+        </div>
+        <div className="flex">
+          <p>Email </p>
+          <p>: {UserData.email}</p>
+        </div>
+        <div className="flex">
+          <p>Phone </p>
+          <p>: {UserData.phone}</p>
+        </div>
+        <div className="flex">
+          <p>Address </p>
+          <p>: {UserData.address.city}</p>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <button
+          type="button"
+          onClick={() => {
+            router.push("/profile/detail");
+          }}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-md"
+        >
+          Back
+        </button>
+      </div>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dataUsers = await res.json();
+  const paths = dataUsers.map((item) => ({
+    params: {
+      id: item.id.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { id } = context.params;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const users = await res.json();
+  return {
+    props: {
+      UserData: users,
+    },
+  };
 }
--- pages/profile/detail.jsx
+++ pages/profile/detail.jsx
@@ -1,9 +1,58 @@
+import { useRouter } from "next/router";
 import Layout from "../../layouts/mainLayout";
 
-export default function detail() {
+export default function detail(props) {
+  const { dataUsers } = props;
+  const router = useRouter();
+
+  // console.log(dataUsers);
   return (
     <Layout pageTitle="Detail User">
-      <h1>User Detail Page</h1>
+      <div className="w-full mx-auto text-center">
+        <h1 className="text-xl m-4 font-semibold">Data User Detail Page</h1>
+        <table className="table-auto w-full border-2 rounded-md">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Address</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataUsers.map((item, index) => (
+              <tr key={item.id}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.address.city}</td>
+                <td>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      router.push(`/profile/${item.id}`);
+                    }}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-md"
+                  >
+                    Detail
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dataUsers = await res.json();
+  return {
+    props: {
+      dataUsers,
+    },
+  };
 }
